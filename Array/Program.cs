40f4713f7058e401c41b14013c454d7c485972fa
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-08-08 17:42:27 -0500

    CG 4-2

--- Array/Program.cs
+++ Array/Program.cs
@@ -5,8 +5,18 @@ namespace Array
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   //Create and initialize in a single line an array with the following values: 1, 1, 2, 3, 5, 8.
+            //Then loop through the array and print out each value.
+
+
+            int[] array = new int []  { 1, 1, 2, 3, 5, 8 };
+
+            foreach (int i in array)
+            //print the array to the console
+
+            Console.WriteLine(i);
+            Console.ReadLine();
+
         }
     }
 }
